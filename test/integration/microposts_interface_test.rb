fe1b5a09614ff2b82d3441a6d76793e162d3b115
Andy Buck <brotherandy@Live.com>
2016-11-02 11:00:49 -0400

    add relationships but get an error trying to migrate

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -15,12 +15,14 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
       post microposts_path, micropost: { content: "" }
     end
     assert_select 'div#error_explanation'
+    assert_select 'input[type=file]'
     # Valid submission
     content = "This micropost really ties the room together"
+    picture = fixture_file_upload('test/fixtures/logoX.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, micropost: { content: content }
+      post microposts_path, micropost: { content: content, picture: picture}
     end
-    assert_redirected_to root_url
+    assert @user.microposts.paginate(page: 1).first.picture?
     follow_redirect!
     assert_match content, response.body
     # Delete a post.
@@ -32,5 +34,19 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     # Visit a different user.
     get user_path(users(:archer))
     assert_select 'a', text: 'delete', count: 0
+  end
+
+  test "micropost sidebar count" do
+    log_in_as(@user)
+    get root_path
+    assert_match "#{@user.microposts.count} microposts", response.body
+    # User with zero microposts
+    other_user = users(:malory)
+    log_in_as(other_user)
+    get root_path
+    assert_match "0 microposts", response.body
+    other_user.microposts.create!(content: "A micropost")
+    get root_path
+    assert_match "1 micropost", response.body
   end
 end
