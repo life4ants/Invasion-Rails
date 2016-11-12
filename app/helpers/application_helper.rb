3c2754deebfc5121367f6c71d6e521caaaaacb5e
Andy Buck <brotherandy@live.com>
2016-11-12 17:37:46 -0500

    html and css for game page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,18 @@
 module ApplicationHelper
 	def full_title(pageTitle = '')
-		baseTitle = "Andy's App"
+		baseTitle = "Invasion"
 		if pageTitle.empty?
 			baseTitle
 		else
 			pageTitle + " | " + baseTitle
+		end
+	end
+
+	def header(controller = 'foo', action = 'bar')
+		if controller == "static_pages" && action == "game"
+			render 'layouts/game_header'
+		else
+			render 'layouts/header'
 		end
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root                 'static_pages#home'
   get    'help'     => 'static_pages#help'
   get    'info'     => 'static_pages#info'
+  get    'game'     =>  'static_pages#game'
   get    'signup'   => 'users#new'
   get    'login'    => 'sessions#new'
   post   'login'    => 'sessions#create'
