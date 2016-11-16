3982b28091f1e16aacfe51b4d8f2335b9add1c02
Andy Buck <brotherandy@live.com>
2016-11-16 17:41:26 -0500

    basic setup for Invasion

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,17 @@
 module ApplicationHelper
 	def full_title(pageTitle = '')
-		baseTitle = "Andy's App"
+		baseTitle = "InvasionS"
 		if pageTitle.empty?
 			baseTitle
 		else
 			pageTitle + " | " + baseTitle
+		end
+	end
+	def header(controller, action)
+		if controller == "static_pages" && action == "game"
+			render 'layouts/game_header'
+		else
+			render 'layouts/header'
 		end
 	end
 end
