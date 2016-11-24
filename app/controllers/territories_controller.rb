2af3a042589a45f4f97b89075a1524412042f37e
Andy Buck <brotherandy@live.com>
2016-11-23 20:13:51 -0500

    delete removed model files

--- app/controllers/territories_controller.rb
+++
@@ -1,2 +0,0 @@
-class TerritoriesController < ApplicationController
-end
--- db/migrate/20161116224834_create_territories.rb
+++
@@ -1,8 +0,0 @@
-class CreateTerritories < ActiveRecord::Migration
-  def change
-    create_table :territories do |t|
-      t.string :name
-      t.text :borders
-    end
-  end
-end
